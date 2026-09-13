b384464fffed8caea18fe167e53dd0aa2a968b69
medishettyvaishnavi <medishettyvaishnavi3526@gmail.com>
2026-09-13 22:33:16 +0530

    Implement JWT authentication

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
@@ -10,11 +11,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+
 app.get("/api/test", (req, res) => {
-    res.json({
-        message: "Smart City API is working",
-    });
+  res.json({
+    message: "Smart City API is working",
+  });
+});
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "You accessed a protected route",
+    user: req.user,
+  });
 });
 
 const PORT = process.env.PORT || 5000;
@@ -22,5 +32,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
